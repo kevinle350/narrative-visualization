a03af51524a3a46d99d7a703c4dc9ea97d4278d2
Kevin Le <kle9293@gmail.com>
2024-07-15 14:14:13 -0700

    Code done

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+// Load the dataset and create the visualization
 d3.json("https://raw.githubusercontent.com/vega/vega-datasets/master/data/cars.json").then(data => {
+    // Filter out data points with null MPG or Horsepower
+    data = data.filter(d => d["Horsepower"] != null && d["Miles_per_Gallon"] != null);
+
     const width = 800;
     const height = 600;
     const margin = { top: 10, right: 10, bottom: 50, left: 60 };
@@ -38,10 +42,19 @@ function setupScene(svg, xScale, yScale, data, filter = "All") {
     const margin = { top: 10, right: 10, bottom: 50, left: 60 };
 
     let filteredData = data;
+    let annotations = [];
     if (filter === "HighMPG") {
         filteredData = data.filter(d => d["Miles_per_Gallon"] > 30);
+        annotations = [{ x: 300, y: 100, text: "Cars with MPG > 30" }];
     } else if (filter === "HighHP") {
         filteredData = data.filter(d => d["Horsepower"] > 150);
+        annotations = [{ x: 500, y: 500, text: "Cars with Horsepower > 150" }];
+    } else {
+        annotations = [
+            { x: 50, y: 40, text: "High MPG, Low Horsepower" },
+            { x: 200, y: 250, text: "Average MPG, Average Horsepower" },
+            { x: 500, y: 550, text: "Low MPG, High Horsepower" }
+        ];
     }
 
     svg.append("g")
@@ -59,6 +72,18 @@ function setupScene(svg, xScale, yScale, data, filter = "All") {
        .attr("cy", d => yScale(d["Miles_per_Gallon"]))
        .attr("r", 5)
        .attr("fill", filter === "HighMPG" ? "orange" : filter === "HighHP" ? "green" : "steelblue");
+
+    svg.selectAll("text.annotation")
+       .data(annotations)
+       .enter()
+       .append("text")
+       .attr("class", "annotation")
+       .attr("x", d => xScale(d.x))
+       .attr("y", d => yScale(d.y))
+       .attr("text-anchor", "start")
+       .attr("font-size", "12px")
+       .attr("fill", "black")
+       .text(d => d.text);
 
     const tooltip = d3.select("body").append("div")
         .attr("class", "tooltip")
